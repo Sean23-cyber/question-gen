6bbc843ee20c03f9274201f300d4cd92825623dc
Sean23-cyber <130288458+Sean23-cyber@users.noreply.github.com>
2025-04-07 22:48:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { router, db } = require('./routes/db_routes');
-const pdf_retrieve=require('./routes/pdf_retrieve.js');
+const {router,db}=require('./routes/pdf_retrieve.js');
 const app = express();
 const PDFDocument = require("pdfkit");
 const mysql = require('mysql');
